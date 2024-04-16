c5351e1247e001f35a9de5368d7f3c016d33518a
pprmint <git@pprmint.art>
2024-04-16 02:14:04 +0200

    Fixing icons and adding delay for volume changes.

--- private_dot_local/private_share/scripts/executable_power-menu.sh
+++ private_dot_local/private_share/scripts/executable_power-menu.sh
@@ -7,7 +7,7 @@ else
     # Sound to make you aware of your surroundings and avoid unintentionally shutting down your puter.
     paplay ~/.local/share/scripts/sounds/Warning.wav &
     # Order: Shutdown, reboot, suspend, exit.
-    options=("" "" "" "")
+    options=("" "" "" "")
 
     selected_option=$(printf '%s\n' "${options[@]}" | tofi --font "lucide" --font-size 20 --prompt-text "" --height 70 --width 206 --corner-radius 35 --text-color "#eee4" --selection-color "#eee" --selection-background "#0000" --horizontal true --result-spacing 15 --text-cursor-corner-radius 0 --text-cursor-thickness 0 --prompt-padding 0 --padding-top 5 --padding-bottom 0 --padding-left 0 --padding-right 0)
     case $selected_option in
@@ -17,10 +17,10 @@ else
         "")
             reboot
             ;;
-        "")
+        "")
             systemctl suspend
             ;;
-        "")
+        "")
             hyprctl dispatch exit
             ;;
     esac
--- private_dot_local/private_share/scripts/executable_volume-control.sh
+++ private_dot_local/private_share/scripts/executable_volume-control.sh
@@ -1,59 +1,83 @@
 #!/bin/sh
+
+# In case you wanna change the default increment for volume.
+vol_increment=0.05
+
+# My keyboard's volume knob is funny; It sends two keystrokes in quick succession when you turn it.
+# This prevents the volume from changing too quickly.
+last_execution_time=$(cat ~/.local/share/scripts/.volume-control-last 2>/dev/null || echo 0)
+current_time=$(date +%s%3N)
+time_since_last_execution=$((current_time - last_execution_time))
+if [ "$time_since_last_execution" -lt 75 ]; then
+    exit 0
+fi
+echo "$current_time" > ~/.local/share/scripts/.volume-control-last
+
+# I use ponymix to show the current volume in dunstify.
 update_volume() {
     current_volume=$(ponymix get-volume)
 }
 
 is_muted=$(wpctl get-volume @DEFAULT_SINK@ | grep -q '\[MUTED\]'; echo $?)
 
+vol_up_notification() {
+    dunstify -a "Volume" -t 2000 -h string:x-canonical-private-synchronous:volume "$current_volume%" -h int:value:"$current_volume" -i ~/.local/share/scripts/icons/volume_up.svg
+}
+
+vol_down_notification() {
+    dunstify -a "Volume" -t 2000 -h string:x-canonical-private-synchronous:volume "$current_volume%" -h int:value:"$current_volume" -i ~/.local/share/scripts/icons/volume_up.svg
+}
+
 mute_notification() {
-    dunstify -a "Volume" -t 2000 -h string:x-canonical-private-synchronous:audio "Muted" -h int:value:"$current_volume" -i ~/.local/share/scripts/icons/volume_mute.svg -u critical
+    dunstify -a "Volume" -t 2000 -h string:x-canonical-private-synchronous:volume "Muted" -h int:value:"$current_volume" -i ~/.local/share/scripts/icons/volume_mute.svg -u critical
 }
 
 case "$1" in
     increase)
         update_volume
+        # Don't wanna go above 100% and blow my speakers, headphones and/or my head up.
         if [ "$current_volume" -eq 100 ]; then
             if [ "$is_muted" -eq 0 ]; then
                 mute_notification
             else
-                dunstify -a "Volume" -t 2000 -h string:x-canonical-private-synchronous:audio "$current_volume%" -h int:value:"$current_volume" -i ~/.local/share/scripts/icons/volume_up.svg
+                vol_up_notification
             fi
         else
-            wpctl set-volume @DEFAULT_AUDIO_SINK@ "0.05+"
+            wpctl set-volume @DEFAULT_AUDIO_SINK@ "$vol_increment+"
             if [ "$is_muted" -eq 0 ]; then
                 mute_notification
                 update_volume
             else
                 update_volume
-                dunstify -a "Volume" -t 2000 -h string:x-canonical-private-synchronous:audio "$current_volume%" -h int:value:"$current_volume" -i ~/.local/share/scripts/icons/volume_up.svg
+                vol_up_notification
                 paplay ~/.local/share/scripts/sounds/Asterisk.wav
             fi
         fi
-        ;;
+    ;;
     decrease)
-        wpctl set-volume @DEFAULT_AUDIO_SINK@ "0.05-"
+        wpctl set-volume @DEFAULT_AUDIO_SINK@ "$vol_increment-"
         update_volume
         if [ "$is_muted" -eq 0 ]; then
             mute_notification
         else
-            dunstify -a "Volume" -t 2000 -h string:x-canonical-private-synchronous:audio "$current_volume%" -h int:value:"$current_volume" -i ~/.local/share/scripts/icons/volume_down.svg
+            vol_down_notification
             paplay ~/.local/share/scripts/sounds/Asterisk.wav
         fi
-        ;;
+    ;;
     toggle-mute)
         if [ "$is_muted" -eq 0 ]; then
             wpctl set-mute @DEFAULT_SINK@ 0
             update_volume
-            dunstify -a "Volume" -t 2000 -h string:x-canonical-private-synchronous:audio "$current_volume%" -h int:value:"$current_volume" -i ~/.local/share/scripts/icons/volume_up.svg
+            vol_up_notification
             paplay ~/.local/share/scripts/sounds/Asterisk.wav
         else
             wpctl set-mute @DEFAULT_SINK@ 1
             update_volume
-            dunstify -a "Volume" -t 2000 -h string:x-canonical-private-synchronous:audio "Muted" -h int:value:"$current_volume" -i ~/.local/share/scripts/icons/volume_mute.svg -u critical
+            mute_notification
         fi
-        ;;
+    ;;
     *)
         echo "Usage: $0 {increase|decrease|toggle-mute}"
         exit 1
-        ;;
+    ;;
 esac
